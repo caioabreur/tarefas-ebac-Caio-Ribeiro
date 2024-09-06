4ec18ce36fc0d0ae6c5a7ae1823ae33ecb68c26e
caio-abreu-r <devcaior@gmail.com>
2024-09-06 17:23:32 -0300

    Criado o projeto e adicionado um Hello + name

---
+++ mod5/TarefaEbac/src/PrimeiraClasse.java
@@ -0,0 +1,10 @@
+public class PrimeiraClasse {
+
+    public static void main(String[] args) {
+
+        // Primeira tarefa da Ebac
+        String name = "Caio";
+
+        System.out.println("Hello " + name);
+    }
+}
